83f9dc3b9293a271ef418fb28fa2e02bcf77d243
hatbann <0urloveis_great@naver.com>
2024-07-21 15:44:23 +0900

    (feat) Change settings

--- zerowavebackend/settings.py
+++ zerowavebackend/settings.py
@@ -18,9 +18,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+from django.core.exceptions import ImproperlyConfigured
+import json
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k48qz4dqo#o%@7ftb(!b=dv#p2(mmd8jt)v2sc2gxk%2h9gah6'
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
